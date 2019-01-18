410b0166d756463aa228a97748d5630be75438fb
Jeffrey B. Daube <jedaube@localhost.localdomain>
2019-01-18 11:39:42 -0500

    check before change laptop

--- Code.js
+++ Code.js
@@ -1,12 +1,55 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 /**
  * Responds to a MESSAGE event in Hangouts Chat.
  *
  * @param {Object} event the event object from Hangouts Chat
  */
-// eslint-disable-next-line no-unused-vars
 function onMessage(event) {
-    var message = 'You just said this: "' + event.message.text + '"';
-    return { text: message };
+    // Determine if user entered any argument text
+    if (event.message.argumentText) {
+
+        // Create variable arg to store users entered text
+        // && trim white-space
+        var arg = event.message.argumentText.trim();
+
+        // Create Regular Expression for integer
+        var integerRegex = new RegExp(/^\d+$/);
+
+        // Is the argument text an integer
+        if (integerRegex.test(arg)) {
+
+            // Create variable to store result off api call
+            var trivia = NumbersApi.getTrivia(arg);
+
+            // Inject result of API call into chat room or DM
+            // using Simple Text Message format
+            return { text: trivia };
+
+        // Argument text is not an integer
+        } else {
+            return { text: getHelpMessage() };
+        }
+    // User did not enter argument text
+    } else {
+        return { text: getHelpMessage() };
+    }
+}
+
+var NumbersApi = {
+    getTrivia: function(arg) {
+
+        // Use Google Apps Script UrlFetchApp.fetch method
+        var response = UrlFetchApp.fetch('http://numbersapi.com/' + arg);
+
+        // Parse the text of the API response
+        var returnResponse = response.getContentText();
+        return returnResponse;
+    }
+};
+
+function getHelpMessage() {
+    return 'Please enter an integer to get it\'s trivia';
 }
 
 /**
@@ -14,9 +57,8 @@ function onMessage(event) {
  *
  * @param {Object} event the event object from Hangouts Chat
  */
-// eslint-disable-next-line no-unused-vars
 function onAddToSpace(event) {
-    return { text: 'Thank you for adding me, " + event.user.displayName + "!' };
+    return { text: 'Thank you for adding me, \' + event.user.displayName + \'!' };
 }
 
 /**
@@ -24,7 +66,7 @@ function onAddToSpace(event) {
  *
  * @param {Object} event the event object from Hangouts Chat
  */
-// eslint-disable-next-line no-unused-vars
+
 function onRemoveFromSpace(event) {
     // eslint-disable-next-line no-console
     console.info('Bot removed from ', event.space.name);
